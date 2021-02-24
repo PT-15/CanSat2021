c85d641749414cb11056beec53eb534996c6a92a
Paula-15 <paulataibo15@gmail.com>
2021-02-24 22:55:29 +0100

    Misión secundaria cámara primera versión

---
+++ misionSecundaria_CASE.py
@@ -0,0 +1,23 @@
+#Más info en https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/2
+
+import sensor
+import time
+import picamera
+
+camera.start_preview()
+
+camera.resolution = (1920,1272)
+camera.annotate_text_size = 18
+camera.annotate_background = Color('blue')
+camera.annotate_foreground = Color('yellow')
+camera.annotate_text = "Las Rozas a 1km de altitud"
+
+lastAlt = None
+alt = sensor.altitud
+variacionMin = None #Poner variación mínima del sensor
+
+while 0 < lastAlt - alt <= variacionMin:
+	camera.capture('/home/pi/Desktop/altitud%0.2.jpg' %sensor.altitud) 
+	lastAlt = sensor.altitud
+	time.sleep (5)
+	alt = sensor.altitud
