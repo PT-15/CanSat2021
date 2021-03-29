32b632c91b99d1873eb2788f1d694906a31d17e6
Samuel Varela <samuelvarela007@gmail.com>
2021-03-29 14:47:51 +0200

    Create Tabladedatos.sql

---
+++ Tabladedatos.sql
@@ -0,0 +1,3 @@
+/* Tabla de datos de tiempo, temperatura, humedad, presión y altitud.*/
+CREATE TABLE Datos recogidos por el satélite (id INTEGER PRIMARY KEY AUTOINCREMENT, Tiempo INTEGER, Temperatura INTEGER, Humedad INTEGER, Presión INTEGER, Altitud INTEGER);
+INSERT INTO Datos recogidos por el satélite VALUES( 
