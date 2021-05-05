845234f512596d81f3d6ef15574a55eaad3027c5
Paula Taibo <paulataibo15@gmail.com>
2021-05-05 14:55:04 +0200

    bugfix gps receiver

--- gpstars.py
+++ gpstars.py
@@ -21,19 +21,22 @@ def main():
 
 			fields = dataLine.split (' ')
 
+			i = 0
+
 			for x in fields:
-				if x <= 5:
-					print(fields[i] + "\n")
-					sensorLog.write(str(fields[x]))
-					if x = 5:
+				if i <= 5:
+					print(x + "\n")
+					sensorLog.write(x)
+					if i == 5:
 						sensorLog.write("\n")
 				else:
-					gpsLog.write(str(fields[x]) + "\n")
+					gpsLog.write(x + "\n")
+					print(x)
 
 			radio.display.fill(0)
 			radio.display.text('%0.2f  P:%0.1f hPa' % (fields[0], fields[3]), 2, 2, 1)
 			radio.display.text('T:%0.1f C  Hum:%0.2f%%' % (fields[1], fields[2]), 2, 13, 1)
-			radio.display.text('Alt:%0.2fm' % (fields{4}), 2, 25, 1)
+			radio.display.text('Alt:%0.2fm' % (fields[4]), 2, 25, 1)
 			radio.display.show()
 
 
