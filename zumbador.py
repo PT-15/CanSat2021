be5f0b79dd1c517204dab62a4a53a7a671beca04
Samuel Varela <samuelvarela007@gmail.com>
2021-05-23 19:42:20 +0200

    Update zumbador.py

--- zumbador.py
+++ zumbador.py
@@ -2,13 +2,14 @@ import zumbadorlibreria.py
 import sensor.py
 import RPi.GPIO as GPIO
 import time
-altura = sensor.altitud()
 altura_dif = 10
 last_altura = sensor.altitud()
 while True:
+    time.sleep(40)
     altura = sensor.altitud()
     diferencia = altura - last_altura
-    if diferencia < altura_dif:
-       GPIO.setmode(GPIO.BOARD)
-        zumbadorlibreria.midi()
-        GPIO.cleanup()
+    if diferencia > 0 :
+        if diferencia < altura_dif:
+            GPIO.setmode(GPIO.BOARD)
+            zumbadorlibreria.midi()
+            GPIO.cleanup()
